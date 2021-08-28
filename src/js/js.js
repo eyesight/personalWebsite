1f1cfe431c660d30bd7a4e8f63fa550c41f06c83
Claudia Friedrich <claudae_f@hotmail.com>
2021-08-28 22:54:00 +0200

    added more color-functions, just trying to get some nice color out of temperature

--- src/js/js.js
+++ src/js/js.js
@@ -7,6 +7,7 @@ class App {
     this.weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=";
     this.region = 'Zurich';
     this.params = `${this.weatherurl}${this.region}&appid=${id}`;
+    this.colorArray = [];
 
     this.clickLeftRight(this.allBtns);
     this.clickOutSide(this.allBtns);
@@ -53,7 +54,7 @@ class App {
     });
   }
 
-  clickOutSide(elements,) {
+  clickOutSide(elements) {
     window.addEventListener("click", (e) => {
       if (!e.target.classList.contains('anchor--inline')) {
         elements.forEach((itemnew) => {
@@ -114,15 +115,18 @@ class App {
     }
 
     let temp = Math.round(weather.main.temp - 273.15); //temperature
-    let weatherid = 100 - Math.round(weather.weather[0].id / 10); //green: weather-code
-    let wind = weather.wind.deg ? Math.round((0.28 * weather.wind.deg) * 2.55) : 90; //blue: winddirection
 
-    let temp2 = Math.round(weather.main.temp - 273.15 + windspeed); //temperature
-    let weatherid2 = Math.round(weather.weather[0].id / 10 + windspeed); //green: weather-code
-    let wind2 = weather.wind.deg ? Math.round((0.28 * weather.wind.deg) * 2.55 + windspeed) : 90 * 2; //blue: winddirection
-
-    let colorBackground = 'rgb(' + weatherid + ', ' + temp + ', ' + wind + ')';
-    let colorFont = 'rgb(' + temp2 + ', ' + wind2 + ', ' + weatherid2 + ')';
+    let colorHsl = this.colorTemperatureToHSL(temp);
+    let colorHex = this.hsl2hex(colorHsl.h, colorHsl.s, colorHsl.l);
+    let colorHex2 = this.hsl2hex(colorHsl.h + 50, colorHsl.s + 20, colorHsl.l - 20);
+    let colorHex3 = this.hsl2hex(colorHsl.h + 100, colorHsl.s, colorHsl.l - 10);
+    this.colorArray.push(colorHex, colorHex2, colorHex3, this.invertColorHex(colorHex));
+    let colorBackground = this.colorArray[0];
+    let colorFont = this.colorArray[2];
+    console.log(temp);
+    console.log(colorHsl);
+    console.log(this.invertColorHex(colorHex));
+    console.log(this.colorArray);
 
     let allTextBlocks = Array.prototype.slice.call(document.querySelectorAll('.text'));
     allTextBlocks.forEach((item) => {
@@ -132,10 +136,8 @@ class App {
     let weathertext = problem ? weather.weather[0].description : weather.weather[0].description + ', temprature: ' + temp + '°C, winddirection: ' + weather.wind.deg + '°, wind speed: ' + weather.wind.speed + ' m/s';
 
     document.querySelector('body').style.backgroundColor = colorBackground;
-
     document.querySelector('.initials').style.color = colorFont;
     document.querySelector('.cursor').style.backgroundColor = colorFont;
-
     document.querySelector('.info').innerHTML = weathertext;
     document.querySelector('.color').innerHTML = colorBackground;
     let x = Math.round(weather.weather[0].id / 10);
@@ -153,6 +155,109 @@ class App {
       document.querySelector('body').classList.add('clouds');
     } else {
       document.querySelector('body').classList.add('default');
+    }
+  }
+
+  invertColorHex(col) {
+    const colors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    let inverseColor = '#'
+    col.replace('#', '').split('').forEach(i => {
+      const index = colors.indexOf(i)
+      inverseColor += colors.reverse()[index]
+    })
+    return inverseColor
+  }
+
+  hsl2hex(h, s, l) {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+  }
+
+  HSLToRGB(h, s, l) {
+    // Must be fractions of 1
+    s /= 100;
+    l /= 100;
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+      m = l - c / 2,
+      r = 0,
+      g = 0,
+      b = 0;
+
+    if (0 <= h && h < 60) {
+      r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+      r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+      r = c; g = 0; b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return {
+      r,
+      g,
+      b
+    };
+  }
+
+  colorTemperatureToHSL(celsius) {
+    let temp = Math.round(celsius);
+    let hue = temp;
+    let saturation = 50;
+    let lightnes = 50;
+
+    switch (true) {
+      case (temp < 40 && temp >= 20):
+        hue = temp + 300;
+        saturation = 80;
+        lightnes = 30;
+        console.log(hue);
+        break;
+      case (temp < 20 && temp >= 10):
+        hue = (((temp - 10) * 10) + 90);
+        saturation = 35;
+        lightnes = 40;
+        console.log(hue);
+        break;
+      case (temp < 10 && temp >= 0):
+        hue = (((10 - temp) * 3) + 180);
+        saturation = 70;
+        lightnes = 30;
+        console.log(hue);
+        break;
+      case (temp < 0):
+        hue = ((temp * -1) + 220);
+        saturation = 100;
+        lightnes = 40;
+        console.log(hue);
+        break;
+      default:
+        hue = 180;
+        saturation = 70;
+        lightnes = 30;
+        console.log(hue);
+        break;
+    }
+
+    return {
+      h: hue,
+      s: saturation,
+      l: lightnes
     }
   }
 }
